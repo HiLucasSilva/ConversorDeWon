f66c59e47237aee82a5791a5e7bdea5077fbe3a9
Lucas Silva <100864919+HiLucasSilva@users.noreply.github.com>
2025-03-31 21:53:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+valorWons = prompt("Digite um valor em Won")
+
+umWonEmReal = 3.91
+umWonEmDolar = 1475.30
+
+alert("O valor convertiodo em Real é: " + valorWons * umWonEmReal)
+
+alert("O valor convertido em Dolar é:" + valorWons * umWonEmDolar)
